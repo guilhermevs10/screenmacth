589599c447dfa4b1f9a8df8fc43b2e881cf0f216
Guilherme Vieira <guilhermebusiness7@gmail.com>
2024-05-18 23:02:07 -0300

    Projeto Spring

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,13 @@
 package br.com.alura.screenmatch;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.model.DadosSerie;
-import br.com.alura.service.ConsumoApi;
-import br.com.alura.service.ConverteDados;
+
+import br.com.alura.principal.Principal;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,12 +18,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-    	var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=e7b642bc");
-    	System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
-
 }
